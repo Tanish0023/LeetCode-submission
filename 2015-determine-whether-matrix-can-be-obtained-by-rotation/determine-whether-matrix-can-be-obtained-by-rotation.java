988382cf301d223f30f5521cf07e1e5bc5b8133f
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2025-05-30 13:54:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (93.99%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,42 +1,25 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
-        // Number of 1 in mat 1
-        int count1 = 0;
-        for(int[] i:mat){
-            for(int j:i){
-                if(j == 1){
-                    count1++;
-                }
-            }
+        if(checkMatrix(mat, target)){
+            return true;
         }
 
-        int count2 = 0;
-        for(int[] i:target){
-            for(int j:i){
-                if(j == 1){
-                    count2++;
+        int rotation = 3;
+        while(rotation > 0){
+            int[][] arr = new int[mat.length][mat.length];
+            for(int i = 0; i < mat.length; i++){
+                for(int j = 0; j < mat.length; j++){
+                    arr[j][mat.length - 1 - i] = mat[i][j];
                 }
             }
-        }
 
-        if(count1 == count2){
-            int rotation = 4;
-            while(rotation > 0){
-                // Rotate 90deg
-                int[][] arr = new int[mat.length][mat.length];
-                for(int i = 0; i < mat.length; i++){
-                    for(int j = 0; j < mat.length; j++){
-                        arr[j][mat.length - 1 - i] = mat[i][j];
-                    }
-                }
-
-                if(checkMatrix(arr, target)){
-                    return true;
-                }
-                mat = arr;
-                rotation--;
+            if(checkMatrix(arr, target)){
+                return true;
             }
+            mat = arr;
+            rotation--;
         }
+        
 
         return false;
     }
