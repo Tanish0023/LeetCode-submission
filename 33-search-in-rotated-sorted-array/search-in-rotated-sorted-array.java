0cbcaf1117c3c698b583b6b4f427737afaaf34c5
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2025-06-01 18:21:26 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (53.30%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -2,13 +2,10 @@ class Solution {
     public int search(int[] nums, int target) {
         int pivot = findPivot(nums);
 
-        // if you did not find a pivot, it means the array is not rotated
         if (pivot == -1) {
-            // just do normal binary search
             return binarySearch(nums, target, 0 , nums.length - 1);
         }
 
-        // if pivot is found, you have found 2 asc sorted arrays
         if (nums[pivot] == target) {
             return pivot;
         }
