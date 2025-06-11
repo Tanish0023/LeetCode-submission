e3a9e75a9b858c0449224825a2feb8b686945e17
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2025-06-11 23:26:00 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (46.50%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,9 +2,9 @@ class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-        int mid;
+    
         while (start <= end) {
-            mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2;
 
             if (nums[mid] < target) {
                 start = mid + 1;
