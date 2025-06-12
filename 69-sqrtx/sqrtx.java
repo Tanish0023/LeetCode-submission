c0185b636a5b9d01e58e3abc5b4dab96d4a1885f
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2025-06-12 23:19:29 +0530

    Time: 1 ms (87.27%) | Memory: 41.1 MB (31.49%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,26 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x < 2) return x; // sqrt(0) = 0, sqrt(1) = 1
+        if (x < 2) return x;
 
-        int left = 0, right = x;
-        int result = 0;
+        int left = 1, right = x / 2;
 
         while (left <= right) {
             int mid = left + (right - left) / 2;
-
-            // To avoid integer overflow, use long for mid * mid
             long square = (long) mid * mid;
 
             if (square == x) {
                 return mid;
             } else if (square < x) {
-                result = mid; // this could be the floor of sqrt(x)
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
 
-        return result;
+        return right;
     }
 }
