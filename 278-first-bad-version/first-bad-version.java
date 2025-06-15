dcf78867ad564a0695cf323f3537a346cafabe4c
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2025-06-15 21:22:39 +0530

    Time: 26 ms (26.54%) | Memory: 39.8 MB (99.57%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -9,16 +9,10 @@ public class Solution extends VersionControl {
         while(start <= end){
             int mid = start + (end - start) / 2;
 
-            if(mid > 1 && isBadVersion(mid) == true && isBadVersion(mid - 1) == false){
-                return mid;
-            }else if(mid == 1 && isBadVersion(mid) == true){
-                return mid;
-            }else if(isBadVersion(mid) == true){
-                end = mid;
-            }else if(isBadVersion(mid) == false){
-                start = mid + 1;
+            if(isBadVersion(mid)){
+                end = mid - 1;
             }else{
-                return mid;
+                start = mid + 1;
             }
         }
 
