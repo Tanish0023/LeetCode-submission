b171f40fc5890cf9ca2dc22a0387fb1c84032f68
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2025-12-14 23:15:54 +0530

    Time: 9 ms (49.30%) | Memory: 22.8 MB (97.24%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -3,7 +3,6 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
-        int ans = 0;
 
         while(low <=  high){
             int mid = low + (high - low) / 2;
@@ -16,12 +15,11 @@ public:
             if(temp > h){
                 low = mid + 1;
             }else{
-                ans = mid;
                 high = mid - 1;
             }
 
         }
 
-        return ans;
+        return low;
     }
 };
