99ce7ff7012582f6862bcc50cc5834e7bd5b921a
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2026-01-14 19:09:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (23.71%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+        if (root == NULL) return ans;
+
+        map<int, int> mp; 
+        queue<pair<TreeNode*, int>> q;
+
+        q.push({root, 0});
+
+        while (!q.empty()) {
+            auto it = q.front();
+            q.pop();
+
+            TreeNode* node = it.first;
+            int level = it.second;
+
+            mp[level] = node->val;
+
+            if (node->left != NULL) {
+                q.push({node->left, level + 1});
+            }
+            if (node->right != NULL) {
+                q.push({node->right, level + 1});
+            }
+        }
+
+        for (auto it : mp) {
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
