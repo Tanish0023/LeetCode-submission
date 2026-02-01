e3ed3eb45ec20d30701e5aea62787cbbf4760ca2
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2026-02-01 19:21:50 +0530

    Time: 61 ms (33.83%) | Memory: 61.8 MB (39.35%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
-    bool isPal(string &s, int l, int r) {
-        while (l < r) {
-            if (s[l] != s[r]) return false;
+    bool isPal(string s, int l, int r){
+        while(l <= r){
+            if(s[l] != s[r]){
+                return false;
+            }
             l++;
             r--;
         }
         return true;
     }
 
-    void rec(int index, string &s,
-             vector<string> &temp,
-             vector<vector<string>> &ans) {
-
-        // ✅ base case
-        if (index == s.length()) {
+    void rec(int index, vector<vector<string>> &ans, vector<string> &temp, string s){
+        if(index == s.length()){
             ans.push_back(temp);
             return;
         }
 
-        // try all substrings starting at index
-        for (int i = index; i < s.length(); i++) {
+        for(int i = index; i < s.length(); i++){
+            if(isPal(s, index, i)){
+                
+                temp.push_back(s.substr(index, i + 1 - index));
 
-            // check palindrome substring
-            if (isPal(s, index, i)) {
+                rec(i + 1, ans, temp, s);
 
-                // take substring
-                temp.push_back(s.substr(index, i - index + 1));
-
-                // recurse for remaining string
-                rec(i + 1, s, temp, ans);
-
-                // backtrack
                 temp.pop_back();
             }
         }
+
     }
 
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> temp;
 
-        rec(0, s, temp, ans);
+        rec(0, ans, temp, s);
+
         return ans;
     }
 };
