c2426e24579e6732a0d6337afd6a3cfaaa32cf0a
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2026-02-11 18:49:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (36.07%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for(int i = 0; i < n / 2; i++){
+            for(int j = i; j < n - i - 1; j++){
+                int temp = matrix[i][j];
+
+                // Bottom to top
+                matrix[i][j] = matrix[n - 1 - j][i];
+
+                // Right to Left
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+
+                // Top to bottom
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+
+                // left to right
+                matrix[j][n - 1 - i] = temp;
+            }
+        }
+    }
+};
