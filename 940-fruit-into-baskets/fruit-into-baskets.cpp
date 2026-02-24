d5a7ba5e5f6dbb1b6172cf02aab643dde8ea5828
Tanish Aggarwal <145790537+Tanish0023@users.noreply.github.com>
2026-02-24 19:37:48 +0530

    Time: 55 ms (15.58%) | Memory: 84.9 MB (35.39%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -8,7 +8,7 @@ public:
         for(int r = 0; r < fruits.size(); r++){
             mp[fruits[r]]++;
 
-            while(mp.size() > 2){
+            if(mp.size() > 2){
                 mp[fruits[l]]--;
 
                 if(mp[fruits[l]] == 0){
